94ea7413305a826ee9520d5a1647419595b52852
Emilie <emmi0002@stud.kea.dk>
2025-01-13 21:38:56 +0100

    tilføjer hover og js til menupunkter

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
+// Musik til videoen
 
-const video = document.querySelector(".video_HeroSection");
+const video = document.querySelector("video_HeroSection");
 
 video.addEventListener("mouseover", () => {
   video.muted = false;
@@ -8,3 +9,38 @@ video.addEventListener("mouseover", () => {
 video.addEventListener("mouseout", () => {
   video.muted = true;
 });
+
+// Ovenstående arrow funktion forkortelse af følgende:
+
+// video.addEventListener("mouseover", unmuteVideo);
+
+// function unmuteVideo() {
+//   video.muted = false;
+// }
+
+// video.addEventListener("mouseout", muteVideo);
+
+// function muteVideo() {
+//   video.muted = true;
+// }
+
+// Funktion til underline af menupunkter
+
+document.querySelector("#indexLink").addEventListener("mouseover", underline1);
+document.querySelector("#portfolioLink").addEventListener("mouseover", underline2);
+document.querySelector("#kompetencerLink").addEventListener("mouseover", underline3);
+
+function underline1() {
+  console.log("underline1");
+  document.querySelector("#indexLink").classList.add("underline_menu");
+}
+
+function underline2() {
+  console.log("underline2");
+  document.querySelector("#portfolioLink").classList.add("underline_menu");
+}
+
+function underline3() {
+  console.log("underline3");
+  document.querySelector("#kompetencerLink").classList.add("underline_menu");
+}
